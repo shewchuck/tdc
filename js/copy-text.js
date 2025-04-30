67365cb91a8576ae727fd09252bdf8f8a4cecd07
Todd <toddunn@gmail.com>
2025-04-30 12:50:16 -0400

    add copy text header and footer
    added copy text to both the header and footer - working

--- js/copy-text.js
+++ js/copy-text.js
@@ -17,3 +17,19 @@ document.getElementById('copy-text-btn').onclick = function() {
 
   })
 }
+document.getElementById('copy-text-btn-top').onclick = function() {
+  const _this = this
+  navigator.clipboard.writeText(document.getElementById('copy-text-top').innerText)
+  .then(function() {
+//     _this.innerText = 'Copied'
+
+//     setTimeout(function() {
+//       _this.innerText = 'Copy'
+//     }, 3000)
+    
+    let toast = document.querySelector('.toast')
+    toast = new bootstrap.Toast(toast)
+    toast.show()
+
+  })
+}
